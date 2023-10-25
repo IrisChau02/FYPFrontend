e167b594e70abe557e828249edae140002baebd0
IrisChau02 <117070549+IrisChau02@users.noreply.github.com>
2023-10-26 01:33:58 +0800

    createGuild

--- layouts/GuildCreate.js
+++ layouts/GuildCreate.js
@@ -5,6 +5,8 @@ import { View, Text, StyleSheet, Image, Pressable, TextInput, TouchableOpacity }
 import axios from 'axios';
 import * as ImagePicker from 'expo-image-picker';
 import BottomBar from "./BottomBar";
+import SelectDropdown from 'react-native-select-dropdown'
+import { Dimensions } from 'react-native';
 
 export default function GuildCreate({ navigation }) {
 
@@ -63,7 +65,7 @@ export default function GuildCreate({ navigation }) {
         if (res.data === 'failed') {
           console.log('No existing record');
         } else {
-          console.log(res.data);
+          //console.log(res.data);
           setDistrictList(res.data)
 
         }
@@ -72,36 +74,152 @@ export default function GuildCreate({ navigation }) {
 
   }, []);
 
+  const handleGuildCreate = () => {
+    console.log(values)
+    /*
+    axios
+      .post(`${process.env.EXPO_PUBLIC_API_BASE_URL}/register`, values)
+      .then((res) => {
+        if (res.data === 'added') {
+          alert('success')
+          navigation.navigate('Login')
+        } else {
+          alert('fail');
+        }
+      })
+      .catch((err) => console.log(err));*/
+  };
+
   return (
     <View style={styles.container}>
       <Image source={PlaceholderImage} style={styles.image} />
       <Text style={styles.heading}>Guild Create Page</Text>
 
-      <TouchableOpacity style={styles.button} onPress={pickImageAsync}>
-        <Text style={styles.buttonText}>Import Guild Logo</Text>
-      </TouchableOpacity>
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button} onPress={pickImageAsync}>
+            <Text style={styles.buttonText}>Import Guild Logo</Text>
+          </TouchableOpacity>
+        </View>
 
-      <View style={styles.imageContainer}>
-        <Image source={values.guildLogo ? { uri: values.guildLogo } : defaultLogoImage}
-          style={{
-            width: 150, height: 150, borderWidth: 3,
-            borderColor: 'grey',
-          }} />
+        <View style={styles.col}>
+          <View style={styles.imageContainer}>
+            <Image source={values.guildLogo ? { uri: values.guildLogo } : defaultLogoImage}
+              style={{
+                width: 150, height: 150, borderWidth: 3,
+                borderColor: 'grey',
+              }} />
+          </View>
+        </View>
       </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Guild Name"
-        value={values.guildName}
-        onChangeText={(text) => handleInputChange('guildName', text)}
-      />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Guild Introduction"
-        value={values.guildIntro}
-        onChangeText={(text) => handleInputChange('guildIntro', text)}
-      />
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Guild Name</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.col}>
+          <TextInput
+            style={styles.input}
+            placeholder="Guild Name"
+            value={values.guildName}
+            onChangeText={(text) => handleInputChange('guildName', text)}
+          />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Guild Introduction</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.col}>
+          <TextInput
+            style={styles.input}
+            placeholder="Guild Introduction"
+            value={values.guildIntro}
+            onChangeText={(text) => handleInputChange('guildIntro', text)}
+            multiline={true}
+            keyboardType="ascii-capable"
+          />
+        </View>
+      </View>
+
+
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Select District</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.col}>
+          <SelectDropdown
+            data={districtList}
+            onSelect={(selectedItem, index) => {
+              //console.log(selectedItem, index);
+
+              setValues({
+                ...values,
+                districtDropdown: selectedItem,
+                districtID: selectedItem?.districtID
+              })
+
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // Return the districtName as the label after item is selected
+              return selectedItem.districtName;
+            }}
+            rowTextForSelection={(item, index) => {
+              // Return the districtName to represent each item in the dropdown
+              return item.districtName;
+            }}
+          />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Level</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.col}>
+          <TextInput
+            style={styles.input}
+            placeholder="Level"
+            value={values.level.toString()}
+          />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.col}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Member</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.col}>
+          <TextInput
+            style={styles.input}
+            placeholder="Member"
+            value={values.member.toString()}
+          />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+      <TouchableOpacity style={styles.button} onPress={handleGuildCreate}>
+          <Text style={styles.buttonText}>Create</Text>
+        </TouchableOpacity>
+      </View>
 
     </View>
   );
@@ -113,6 +231,20 @@ const styles = StyleSheet.create({
     //alignItems: 'center',
     //justifyContent: 'center',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+
+  },
+  col: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    //margin: 10,
+    width: Dimensions.get('window').width / 2,
+    justifyContent: 'center'
+  },
+
   image: {
     width: '100%',
     height: '100%',
@@ -130,7 +262,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     padding: 10,
     borderRadius: 5,
-    marginTop: 20,
+    width: 150,
+    //margin: 20,
   },
   buttonText: {
     color: '#fff',
@@ -141,10 +274,10 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   input: {
+    width: 150,
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 10,
     paddingHorizontal: 8,
     backgroundColor: 'lightgray', // Set the background color
   },
